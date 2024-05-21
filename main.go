093883368abc8c6573e89fc65a77a72e141c5230
Manoel Fernando C. Bentes <mfcbentes.stm@gmail.com>
2024-05-21 15:24:22 -0300

    :sparkles: feat: conexão com mysql

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"database/sql"
 
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	DBHOST = "DBHOST"
+	DBUSER = "DBUSER"
+	DBPASS = "DBPASS"
+	DBNAME = "DBNAME"
+)
+
+var db *sql.DB
+
+func main() {
+	_, err := sql.Open("mysql", DBUSER+":"+DBPASS+"@tcp("+DBHOST+")/"+DBNAME+"")
+	if err != nil {
+		panic(err)
+	}
+
+	println("conectado!")
 }
